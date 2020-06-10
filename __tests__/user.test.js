17a693f9d6b96ef2e01fa6638ee0fe28dc6250b3
Logan Scott <logantscott@gmail.com>
2020-06-10 08:09:25 -0600

    add user test passes - if using select in route, rest does not return, including _v

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -5,6 +5,7 @@ const connect = require('../lib/utils/connect');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
 describe('voting-app routes', () => {
   beforeAll(async() => {
@@ -21,7 +22,21 @@ describe('voting-app routes', () => {
     return mongod.stop();
   });
 
-  it('does a thing', () => {
-
+  // Create a user
+  it('can create a note with execute', () => {
+    return User.create({
+      name: 'Logan Scott',
+      phone: '123 456 7890',
+      email: 'email@email.com',
+      communicationMedium: 'email',
+      imageUrl: 'placekitten.com/400/400'
+    })
+      .then(() => request(app).get('/api/v1/user'))
+      .then(users => {
+        expect(users.body).toEqual([{
+          _id: expect.anything(),
+          name: 'Logan Scott'
+        }]);
+      });
   });
 });
