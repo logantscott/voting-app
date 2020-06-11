5e72a1aabb7e6306da8b5c96c9371c7eb01d3e4d
Logan Scott <logantscott@gmail.com>
2020-06-11 01:42:08 -0600

    poll first test failing... committing before debugging

--- __tests__/poll.test.js
+++ __tests__/poll.test.js
@@ -6,6 +6,7 @@ const connect = require('../lib/utils/connect');
 const request = require('supertest');
 const app = require('../lib/app');
 const Poll = require('../lib/models/Poll');
+const Organization = require('../lib/models/Organization');
 
 describe('voting-app routes', () => {
   beforeAll(async() => {
@@ -24,15 +25,36 @@ describe('voting-app routes', () => {
 
   // create a poll
   it('can create a poll', () => {
-    return request(app)
-      .post('/api/v1/poll')
-      .send({
-
-      })
+    return Organization.create({
+      title: 'A New Org',
+      description: 'this is a very cool org',
+      imageUrl: 'placekitten.com/400/400'
+    })
+      .then(organization => request(app)
+        .post('/api/v1/polls')
+        .send({
+          organization: organization._id,
+          title: 'This is a new poll',
+          description: 'I am the description of this poll',
+          options: [
+            'Option 1',
+            'Option 2',
+            'Option 3',
+            'Option 4'
+          ]
+        }))
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
-
+          organization: expect.any(mongoose.Schema.Types.ObjectId),
+          title: 'This is a new poll',
+          description: 'I am the description of this poll',
+          options: [
+            'Option 1',
+            'Option 2',
+            'Option 3',
+            'Option 4'
+          ],
           __v: 0
         });
       });
