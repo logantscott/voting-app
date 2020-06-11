19cb77a164e89aa941ba485b9105a82b0972220e
Logan Scott <logantscott@gmail.com>
2020-06-11 06:44:24 -0600

    can find memberships by user/org and can delete tests passing

--- __tests__/membership.test.js
+++ __tests__/membership.test.js
@@ -58,4 +58,77 @@ describe('voting-app routes', () => {
         });
       });
   });
+
+  // get all users in an organization
+  it('can get all users in an organization', () => {
+    return Membership
+      .create({
+        organization: organization._id,
+        user: user._id
+      })
+      .then(() => request(app)
+        .get(`/api/v1/memberships?organization=${organization.id}`))
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.anything(),
+          organization: {
+            _id: organization.id,
+            title: organization.title,
+            imageUrl: organization.imageUrl
+          },
+          user: {
+            _id: user.id,
+            name: user.name,
+            imageUrl: user.imageUrl
+          },
+          __v: 0
+        }]);
+      });
+  });
+
+  // get all organizations a user is part of
+  it('can get all organizations a user is part of', () => {
+    return Membership
+      .create({
+        organization: organization._id,
+        user: user._id
+      })
+      .then(() => request(app)
+        .get(`/api/v1/memberships?user=${user.id}`))
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.anything(),
+          organization: {
+            _id: organization.id,
+            title: organization.title,
+            imageUrl: organization.imageUrl
+          },
+          user: {
+            _id: user.id,
+            name: user.name,
+            imageUrl: user.imageUrl
+          },
+          __v: 0
+        }]);
+      });
+  });
+
+  // delete a membership
+  it('can delete a membership', () => {
+    return Membership
+      .create({
+        organization: organization._id,
+        user: user._id
+      })
+      .then(membership => request(app)
+        .delete(`/api/v1/memberships/${membership.id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          organization: organization.id,
+          user: user.id,
+          __v: 0
+        });
+      });
+  });
 });
