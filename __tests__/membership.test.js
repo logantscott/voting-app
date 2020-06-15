80b0adddf2407ed6722bf1e8e8d4eec66cb4d049
Logan Scott <logantscott@gmail.com>
2020-06-15 08:19:40 -0600

    delete votes associated with a membership org/user pair - poll owned by org and vote owned by user - test passes

--- __tests__/membership.test.js
+++ __tests__/membership.test.js
@@ -8,6 +8,8 @@ const app = require('../lib/app');
 const Membership = require('../lib/models/Membership');
 const User = require('../lib/models/User');
 const Organization = require('../lib/models/Organization');
+const Vote = require('../lib/models/Vote');
+const Poll = require('../lib/models/Poll');
 
 describe('voting-app routes', () => {
   beforeAll(async() => {
@@ -129,6 +131,104 @@ describe('voting-app routes', () => {
           user: user.id,
           __v: 0
         });
+      });
+  });
+});
+
+describe('voting-app routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let organization, user;
+  beforeEach(async() => {
+    organization = await Organization.create({
+      title: 'A New Org',
+      description: 'this is a very cool org',
+      imageUrl: 'placekitten.com/400/400'
+    });
+
+    user = await User.create({
+      name: 'Logan Scott',
+      phone: '123 456 7890',
+      email: 'email@email.com',
+      communicationMedium: 'email',
+      imageUrl: 'placekitten.com/400/400'
+    });
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  // delete a membership and all votes by user
+  it('can delete a membership and all votes by user', async() => {
+
+    const membership = await Membership
+      .create({
+        organization: organization._id,
+        user: user._id
+      });
+
+    const polls = await Poll
+      .create([
+        {
+          organization: organization._id,
+          title: 'Poll 1',
+          description: 'I am the description of this poll',
+          options: [{ option: 'Option 1' }, { option: 'Option 2' }, { option: 'Option 3' }, { option: 'Option 4' }]
+        },
+        { // votes on this poll should not get deleted
+          organization: mongoose.Types.ObjectId(),
+          title: 'Poll 2',
+          description: 'I am the description of this poll',
+          options: [{ option: 'Option 1' }, { option: 'Option 2' }, { option: 'Option 3' }, { option: 'Option 4' }]
+        },
+        {
+          organization: organization._id,
+          title: 'Poll 3',
+          description: 'I am the description of this poll',
+          options: [{ option: 'Option 1' }, { option: 'Option 2' }, { option: 'Option 3' }, { option: 'Option 4' }]
+        }
+      ]);
+
+    await Vote
+      .create([
+        {
+          poll: polls[0]._id,
+          user: user._id,
+          option: mongoose.Types.ObjectId()
+        },
+        {
+          poll: polls[1]._id,
+          user: user._id,
+          option: mongoose.Types.ObjectId()
+        },
+        {
+          poll: polls[2]._id,
+          user: user._id,
+          option: mongoose.Types.ObjectId()
+        }
+      ]);
+    
+    return request(app)
+      .delete(`/api/v1/memberships/${membership.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          organization: organization.id,
+          user: user.id,
+          __v: 0
+        });
+
+        return Vote.find({ user: user.id, poll: { $in: [polls[0]._id, polls[2]._id] } })
+          .then(res => expect(res).toEqual([]));
       });
   });
 });
